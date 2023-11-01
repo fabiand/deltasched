eb948a7e724aeee5cfed82f91557a3adb79ebd29
Fabian Deutsch <fabiand@fedoraproject.org>
2023-11-01 12:59:36 +0100

    mod cli
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ src/cli.rs
@@ -0,0 +1,45 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, default_value="yaml")]
+    pub output: OutputFormat,
+
+    #[command(subcommand)]
+    pub command: Commands
+}
+
+#[derive(ValueEnum, Debug, Clone)]
+pub enum OutputFormat {
+    Yaml,
+    Human/*,
+    MermaidGantt*/
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Output an example schedule skeleton
+    Example {},
+
+    /// Create a new schedule
+    New {
+        /// Name of the project release
+        #[arg(long)]
+        name: String,
+        /// The schedule skeleton to use
+        #[arg(long)]
+        from_skeleton: String,
+        /// The known dates, a tuple of <milestone alias>:<yyyy-mm-dd>
+        #[arg(long)]
+        with_due_date: String
+    },
+
+    /// Replan an existing schedule
+    /// It is expected that to be replanned dates are null
+    Replan {
+        #[arg(long)]
+        schedule: String
+    }
+}
+
