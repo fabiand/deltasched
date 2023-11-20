5b4244e79e3b8b7b43dbb1900e084df87848d4b0
Fabian Deutsch <fabiand@fedoraproject.org>
2023-11-20 11:27:43 +0100

    Make it work again
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/printer.rs
+++ src/printer.rs
@@ -16,67 +16,72 @@ impl ScheduleFormatter {
 }
 
 pub struct TextSchedule {
-    text: Vec<String>
 }
 
 impl TextSchedule {
     pub fn new() -> TextSchedule {
         TextSchedule {
-            text: Vec::new()
         }
     }
     pub fn format(&mut self, doc: &Document) -> String {
-        self.visit_document(&doc);
-        self.text.concat()
+        self.visit_document(&doc)
     }
 
-    fn visit_document(&mut self, doc: &Document) { 
-        self.text.push(format!("# Kind"));
-        self.text.push(format!("kind: {}\n", &doc.kind));
-        self.text.push(format!("# Metadata"));
-        self.text.push(format!("{}", serde_yaml::to_string(&doc.metadata).unwrap()));
-        self.text.push(format!("# Spec"));
-        self.visit_schedule(&doc.spec);
-        self.text.push(format!("# Status"));
-        self.text.push(format!("## Phases & Milestones"));
+    fn visit_document(&mut self, doc: &Document) -> String {
+        let mut text = Vec::new();
+        text.push(format!("# Kind"));
+        text.push(format!("kind: {}\n", &doc.kind));
+        text.push(format!("# Metadata"));
+        text.push(format!("{}", serde_yaml::to_string(&doc.metadata).unwrap()));
+        text.push(format!("# Spec"));
+        text.push(self.visit_schedule(&doc.spec));
+        text.push(format!("\n# Status"));
+        text.push(format!("## Phases & Milestones"));
         if doc.status.is_some() {
             for p in &doc.status.as_ref().unwrap().phases {
-                self.visit_phase(&p);
+                text.push(String::from("\n"));
+                text.push(self.visit_phase(&p));
             }
         }
+        text.join("\n")
     }
 
-    fn visit_milestone(&mut self, m: &Milestone) {
+    fn visit_milestone(&mut self, m: &Milestone) -> String {
         let due_date = if m.due_date.is_none()
           { String::from("---------- ---") }
           else
           { format!("{}", m.due_date.unwrap().format("%Y-%m-%d %a")) };
-          self.text.push(format!(" {}   {}   {}", due_date, m.alias, m.name));
+        format!(" {}   {}   {}", due_date, m.alias, m.name)
     }
 
-    fn visit_milestone_generator(&mut self, m: &MilestoneGenerator) {
-        self.text.push(format!("{}x {}", m.count, m.name));
+    fn visit_milestone_generator(&mut self, m: &MilestoneGenerator) -> String {
+        format!("{}x {}", m.count, m.name)
     }
 
-    fn visit_phase(&mut self, p: &Phase) {
-        self.text.push(format!("{}", p.name));
+    fn visit_phase(&mut self, p: &Phase) -> String {
+        let mut text = Vec::new();
+        text.push(format!("{}", p.name));
         for ms in &p.milestones {
-            self.visit_milestone(&ms);
+            text.push(self.visit_milestone(&ms));
         };
+        text.join("\n")
     }
 
-    fn visit_schedule(&mut self, s: &Schedule) {
-        self.text.push(format!("## Phases & Milestones"));
+    fn visit_schedule(&mut self, s: &Schedule) -> String {
+        let mut text = Vec::new();
+        text.push(format!("## Phases & Milestones"));
         for p in &s.phases {
-            self.visit_phase(&p);
+            text.push(String::from("\n"));
+            text.push(self.visit_phase(&p));
         }
 
-        self.text.push(format!("## Baseline Deltas"));
+        text.push(format!("## Baseline Deltas"));
         // Note: We are reverting the order and to/from in order to
         // make it ore natural to read the output
         // The output will be from oldest to most recent
         for d in s.milestone_deltas.iter().rev() {
-            self.text.push(format!("- {} {} {}: {}", d.milestone, d.is, d.target, d.by));
+            text.push(format!("- {} {} {}: {}", d.milestone, d.is, d.target, d.by));
         }
+        text.join("\n")
     }
 }
